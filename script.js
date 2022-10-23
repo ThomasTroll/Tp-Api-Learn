eeb03fe0f901562222047f53b9d9f3a41c492bed
ThomasTroll <1337@mailfence.com>
2022-10-23 20:57:07 +0200

    version bof bof mais a rendre

--- script.js
+++ script.js
@@ -6,22 +6,21 @@ const place = document.querySelector(".place");
 const sendValue = () => {
     const myKey = '4a3d98b3184f30861b855fece8afc03f';
     const city = searchText.value;
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric$lang=fr&appid=${myKey}`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${myKey}`)
         .then((res) => res.json())
         .then((data) => {
             place.innerHTML =`
       <div class="data_results">
-      
-        <img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="meteo d'aujourd'hui dans ${data.name}"/>
+      <img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="meteo${data.name}"/>
           <h3>${data.name}</h3>
-         ${data.main.temp} °C
-          <p>${data.weather[0].description}</p>
+        <h4> ${data.main.temp} °C</h4>
           <div class="data">
             <div class="temp">
               <div class="data_value">  Min :
                 <span class="data_num">${data.main.temp_min} °C</span>
               </div>
-              <div class="data_value">Max :${data.main.temp_max} °C</span>
+              <div class="data_value">Max :
+<span class="data_num"> ${data.main.temp_max} °C</span>
               </div>
             </div>
             <div class="weather">
@@ -41,4 +40,11 @@ const sendValue = () => {
         });
 };
 
-submitBtn.addEventListener("click", sendValue);
+submitBtn.addEventListener("click", sendValue)
+
+
+searchText.addEventListener("keyup", function(e){
+    if(e.key ==="Enter"){
+        sendValue(searchText.value)
+    }
+})
